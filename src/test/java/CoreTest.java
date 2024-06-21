aafb771acec7efadb4d6774e616dd435be393339
Dmitrii Volkov <dmitryforbusiness@yandex.ru>
2024-06-21 15:51:53 +0300

    update commit

--- src/test/java/CoreTest.java
+++ src/test/java/CoreTest.java
@@ -3,17 +3,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class CoreTest {
     WebDriver driver;
 
     @Before
-    public void beforeTest(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void beforeTest() {
+        String browser = System.getProperty("browser", "chrome");
+        setupDriver(browser);
         driver.get("https://qa-scooter.praktikum-services.ru/");
     }
 
+    public void setupDriver(String browser) {
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+    }
 
     @After
     public void tearDown(){
